afa692b9d2438d945f89789f597cefcbbd5e12f5
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-16 18:47:22 +0600

    app.js add and selected option add

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const BASE_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currenxies/usd/bdt.json";
 
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "BDT") {
+      newOption.selected = "selected";
+      select.append(newOption);
+    }
+  }
+}
