3a47ab9f1b223839aba18efce70d59490e9d8fd0
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-16 19:21:32 +0600

    flag name add

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currenxies/usd/bdt.json";
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currenxies/";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
 
 for (let select of dropdowns) {
   for (currCode in countryList) {
@@ -12,7 +15,31 @@ for (let select of dropdowns) {
       newOption.selected = "selected";
     } else if (select.name === "to" && currCode === "BDT") {
       newOption.selected = "selected";
-      select.append(newOption);
     }
+    select.append(newOption);
   }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
 }
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  console.log(newSrc);
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const URL = `${Base_URL}/${fromCurr}/${toCurr}.json `;
+});
